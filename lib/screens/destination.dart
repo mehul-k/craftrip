b01dd1c96c09a93fedefa1d66e0810b47b311b6c
Anusha Datta <anushadatta@gmail.com>
2020-03-10 01:31:19 +0800

    Travel card.

---
+++ lib/screens/destination.dart
@@ -0,0 +1,19 @@
+class Destination
+{
+  String city;
+  String country;
+  double temperature;
+  double exchangeRate;
+  String currency;
+  bool favourite;
+  String imageURL;
+
+  Destination({
+    this.city,
+    this.country,
+    this.temperature,
+    this.exchangeRate,
+    this.currency,
+    this.favourite,
+    this.imageURL });
+}
---
+++ lib/screens/destination_card.dart
@@ -0,0 +1,142 @@
+import 'package:flutter/material.dart';
+import 'destination.dart';
+
+class DestinationCard extends StatefulWidget {
+  @override
+  Destination d;
+  DestinationCard({this.d});
+
+  @override
+  _DestinationCardState createState() => _DestinationCardState();
+}
+
+class _DestinationCardState extends State<DestinationCard> {
+  Widget build(BuildContext context) {
+
+    // Initialising favourite button for error handling    
+    if(widget.d.favourite == null)
+      widget.d.favourite = false; 
+
+    return Container(
+
+      height: 160, 
+
+      child: Card(
+        margin: EdgeInsets.fromLTRB(16, 16, 16, 0),
+        color: Colors.grey[300],
+
+        child: Padding(
+          padding: const EdgeInsets.all(12.0),
+
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+
+            children: <Widget>[
+
+              // Destination Image 
+              Padding(
+                padding: const EdgeInsets.fromLTRB(0, 0, 12.0, 0),
+                child: Image.network(widget.d.imageURL, height:110),
+              ),
+              
+              // Destination information (City, country, exchange rate and temperature) 
+              Expanded(
+                child: Column(
+ 
+                  mainAxisSize: MainAxisSize.min,
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: <Widget>[
+
+                    Center(
+                      child: Text( widget.d.city +', ' + widget.d.country, 
+                      style: TextStyle(
+                        fontSize: 18.0,
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold,
+                      ),),
+                    ),
+
+                    SizedBox(height: 7.0),
+
+                    Center(
+                      child: Text( "Temperature", 
+                        style: TextStyle(
+                        fontSize: 14.0,
+                        color: Colors.black,
+                        
+                      ),),
+                    ),
+
+                    SizedBox(height: 2.0),
+
+                    Center(
+                      child: Text(widget.d.temperature.toString() + "°C", 
+                        style: TextStyle(
+                        fontSize: 14.0,
+                        color: Colors.black,
+                        
+                      ),),
+                    ),
+
+                    SizedBox(height: 3.0),
+
+                    Center(
+                      child: Text( "Exchange", 
+                        style: TextStyle(
+                        fontSize: 14.0,
+                        color: Colors.black,
+                        
+                      ),),
+                    ),
+
+                    SizedBox(height: 2.0),
+
+                    Center(
+                      child: Text("1SGD = " + widget.d.exchangeRate.toString() + widget.d.currency, 
+                        style: TextStyle(
+                        fontSize: 14.0,
+                        color: Colors.black,
+                        
+                      ),),
+                    ),
+
+                  ],
+                ),
+              ),
+              
+              // Favourite button 
+              Container(
+                height: 100,
+                child: Align(
+                  alignment: Alignment.centerRight,
+                  child: Ink(
+                    decoration: const ShapeDecoration(
+                      color: Colors.black87,
+                      shape: CircleBorder(),
+                    ),
+                    width: 30.0,
+                    height: 30.0,
+                    child: IconButton(
+                      icon: Icon(Icons.favorite),
+                      color: widget.d.favourite ? Colors.pinkAccent[400] : Colors.white,
+                      iconSize: 13.0,
+                      onPressed: () {
+                        setState(() {
+                          if(widget.d.favourite == true)
+                            widget.d.favourite = false;
+                          else
+                            widget.d.favourite = true;   
+                        });
+                      },
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
