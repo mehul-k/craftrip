b2426640f6511688582d5a8160743de608c1337a
Amrita <amritaravishankar00@gmail.com>
2020-03-22 17:44:50 +0800

    import statement change

--- lib/summary.dart
+++ lib/summary.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'dart:convert';
 import 'package:http/http.dart' as http;
-import './weather.dart'; //import WeatherData
+import 'package:craftrip_app/models/weather.dart'; //import WeatherData
 
 class Summary extends StatefulWidget {
 
