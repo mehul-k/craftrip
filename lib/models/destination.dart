6dcab0bde80d1f07227c54ff321f26ba21fbdbad
Mehul <mehul.kumar171@gmail.com>
2020-03-28 14:32:28 +0800

    bug fix

--- lib/models/destination.dart
+++ lib/models/destination.dart
@@ -7,6 +7,18 @@ class Destination
   String currency;
   bool favourite;
   String imageURL;
+  bool bucketAdventure;
+  bool bucketCalmAtmosphere;
+  bool bucketCoastalLandscape;
+  bool bucketEntertainment;
+  bool bucketLandmark;
+  bool bucketVibrantAtmosphere;
+  bool bucketUrbanLandscape;
+  bool bucketMountainousLandscape;
+  bool bucketHistoricalArchitecture;
+  String tag1;
+  String tag2;
+  String tag3;
 
   Destination({
     this.city,
@@ -15,5 +27,17 @@ class Destination
     this.exchangeRate,
     this.currency,
     this.favourite,
-    this.imageURL });
+    this.imageURL,
+    this.bucketAdventure,
+    this.bucketCalmAtmosphere,
+    this.bucketCoastalLandscape,
+    this.bucketEntertainment,
+    this.bucketLandmark,
+    this.bucketVibrantAtmosphere,
+    this.bucketUrbanLandscape,
+    this.bucketMountainousLandscape,
+    this.bucketHistoricalArchitecture,
+    this.tag1,
+    this.tag2,
+    this.tag3});
 }
--- lib/services/collections.dart
+++ lib/services/collections.dart
@@ -9,7 +9,8 @@ class Collections
   final databaseReference = Firestore.instance;
 
   String userID = LoginModel.userID;
-  String emailID = LoginModel.emailID; 
+  String emailID = LoginModel.emailID;
+  static var userBucketTag;
   
   addToFavourites(Destination d) async {
 
@@ -165,14 +166,74 @@ class Collections
                   city: d['City'],
                   country: d['Country'],
                   currency: d['Currency'],
-                  imageURL: d['image'])
-          )
-      );
+                  imageURL: d['image'],
+                  bucketAdventure: d['Bucket_Adventure'],
+                  bucketCalmAtmosphere: d['Bucket_Calm_Atmosphere'],
+                  bucketCoastalLandscape: d['Bucket_Coastal_Landscape'],
+                  bucketEntertainment: d['Bucket_Entertainment'],
+                  bucketLandmark: d['Bucket_Landmark'],
+                  bucketVibrantAtmosphere: d['Bucket_Vibrant_Atmosphere'],
+                  bucketUrbanLandscape: d['Bucket_Urban_Landscape'],
+                  bucketMountainousLandscape: d['Bucket_Mountainous_Landscape'],
+                  bucketHistoricalArchitecture: d['Bucket_Historical_Architecture'],
+                  tag1: d['Tag1'],
+                  tag2: d['Tag2'],
+                  tag3: d['Tag3'])),
+          );
     });
-    return travelDestinations;
+
+    List<Destination> preferredDestinations = [];
+    List<Destination> remainingDestinations = [];
+
+    for(int i=0; i<travelDestinations.length; i++){
+
+      if(travelDestinations[i].bucketAdventure == true && userBucketTag=='Bucket_Adventure')
+      {
+        preferredDestinations.add(travelDestinations[i]);
+      }
+      else if(travelDestinations[i].bucketCalmAtmosphere == true && userBucketTag=='Bucket_Calm_Atmosphere')
+      {
+        preferredDestinations.add(travelDestinations[i]);
+      }
+      else if(travelDestinations[i].bucketCoastalLandscape == true && userBucketTag=='Bucket_Coastal_Landscape')
+      {
+        preferredDestinations.add(travelDestinations[i]);
+      }
+      else if(travelDestinations[i].bucketLandmark == true && userBucketTag=='Bucket_Landmark')
+      {
+        preferredDestinations.add(travelDestinations[i]);
+      }
+      else if(travelDestinations[i].bucketVibrantAtmosphere == true && userBucketTag=='Bucket_Vibrant_Atmosphere')
+      {
+        preferredDestinations.add(travelDestinations[i]);
+      }
+      else if(travelDestinations[i].bucketUrbanLandscape == true && userBucketTag=='Bucket_Urban_Landscape')
+      {
+        preferredDestinations.add(travelDestinations[i]);
+      }
+      else if(travelDestinations[i].bucketMountainousLandscape == true && userBucketTag=='Bucket_Mountainous_Landscape')
+      {
+        preferredDestinations.add(travelDestinations[i]);
+      }
+      else if(travelDestinations[i].bucketHistoricalArchitecture == true && userBucketTag=='Bucket_Historical_Architecture')
+      {
+        preferredDestinations.add(travelDestinations[i]);
+      }
+      else if(travelDestinations[i].bucketEntertainment == true && userBucketTag=='Bucket_Entertainment')
+      {
+        preferredDestinations.add(travelDestinations[i]);
+      }
+      else{
+        remainingDestinations.add(travelDestinations[i]);
+      }
+
+    }
+
+    preferredDestinations.addAll(remainingDestinations);
+
+    return preferredDestinations;
   }
 
-  Future<List<String>> getUserInfo() async{
   createBucketTag() async{
     await databaseReference.collection("users")
       .document(userID)
@@ -189,6 +250,7 @@ class Collections
     });}catch(e){}
   }
 
+  Future<List<String>> getUserInfo() async{
     List<String> userInfo = [];
     String firstname, lastname, name; 
 
