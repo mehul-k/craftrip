1f15858f3eddc1d2ff3c8e62db2961b69e240096
Sarah Yeoh <57384927+SYEOH003@users.noreply.github.com>
2020-03-28 17:58:16 +0800

    Exchange MVC and Google Map added

---
+++ lib/models/MapData.dart
@@ -0,0 +1,111 @@
+
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+class MapData{
+
+  //Data({this.mar1, this.mar2, this.mar3, this.mar4, this.mar5, this.mar6, this.mar7, this.mar8, this.mar9, this.mar10, this.mar11});
+
+  Marker mar1 = Marker(
+        markerId: MarkerId('AL ABRAR'),
+        draggable: false,
+        infoWindow: InfoWindow(title: 'AL ABRAR', snippet: '1 Harbourfront Walk #02-171 S(98585), Vivo City'),
+        position: LatLng(1.265040, 103.821760));
+
+  Marker mar2 = Marker(
+        markerId: MarkerId('DOLLAR EXCHANGE PTE LTD'),
+        draggable: false,
+        infoWindow: InfoWindow(title: 'DOLLAR EXCHANGE PTE LTD', snippet: '63 Jurong West Central 3 #01-18 S(648331)'),
+        position: LatLng(1.33936402101721,103.705308192584));
+    
+  Marker mar3 = Marker(
+        markerId: MarkerId('Rohail Exchange Pte Ltd'),
+        draggable: false,
+        infoWindow: InfoWindow(title: 'Rohail Exchange Pte Ltd', snippet: '70 Woodlands Avenue 7 #02-K12 S(738344)'),
+        position: LatLng(1.440040,103.798531));
+
+  Marker mar4 = Marker(
+        markerId: MarkerId('RAHIM MONEY CHANGER'),
+        draggable: false,
+        infoWindow: InfoWindow(title: 'RAHIM MONEY CHANGER', snippet: '2 Marina Boulevard #B1-01 S(018987)'),
+        position: LatLng(1.44053815128592,103.80097758134));
+
+  Marker mar5 = Marker(
+        markerId: MarkerId('ROSEMIN STORE'),
+        draggable: false,
+        infoWindow: InfoWindow(title: 'ROSEMIN STORE', snippet: '175 Bencoolen Street #01-32 S(189642)'),
+        position: LatLng(1.30165915571756,103.852237533762));
+    
+   Marker mar6 = Marker(
+        markerId: MarkerId('RANITHA TRADING'),
+        draggable: false,
+        infoWindow: InfoWindow(title: 'RANITHA TRADING', snippet: '10 Tampines Central 1 #B1-23 S(529536)'),
+        position: LatLng(1.35431555986081,103.945019461564));
+    
+    Marker mar7 = Marker(
+        markerId: MarkerId('A.R.MONEY EXCHANGE'),
+        draggable: false,
+        infoWindow: InfoWindow(title: 'A.R.MONEY EXCHANGE', snippet: '9 Bishan Place #01-K10 S(579837)'),
+        position: LatLng(1.35023942729659,103.848477635059));
+
+    Marker mar8 = Marker(
+        markerId: MarkerId('CURRENCY EXCHANGE'),
+        draggable: false,
+        infoWindow: InfoWindow(title: 'CURRENCY EXCHANGE', snippet: '704 Ang Mo Kio Avenue 8 #01-2551 S(560704)'),
+        position: LatLng(1.37040711662241,103.847230726388));
+
+    Marker mar9 = Marker(
+        markerId: MarkerId('AJM Money Exchange Pte Ltd'),
+        draggable: false,
+        infoWindow: InfoWindow(title: 'CURRENCY EXCHANGE', snippet: '430 Upper Changi Road #01-39 East Village S(487048)'),
+        position: LatLng(1.33177580673345,103.946201248988));
+    
+    Marker mar10 = Marker(
+        markerId: MarkerId('ALIZIA GIFT HOUSE'),
+        draggable: false,
+        infoWindow: InfoWindow(title: 'CURRENCY EXCHANGE', snippet: '10 Sinaran Drive #B1-85 Square 2 S(307506)'),
+        position: LatLng(1.32042966145248,103.843892131406));
+
+    Marker mar11 = Marker(
+        markerId: MarkerId('GOLDEN DRAGON MONEY CHANGER'),
+        draggable: false,
+        infoWindow: InfoWindow(title: 'CURRENCY EXCHANGE', snippet: 'Chinatown Point, 133 New Bridge Road #02-24 S(059413)'),
+        position: LatLng(1.28499882771298,103.844697085395));
+
+    Marker mar12 = Marker(
+      markerId: MarkerId('BEZ IMPEX PTE LTD'),
+      draggable: false,
+      infoWindow: InfoWindow(title: 'BEZ IMPEX PTE LTD', snippet: 'Boon Lay Shopping Centre, 221 Boon Lay Place #01-128 S(640221)'),
+      position: LatLng(1.34634818389509,103.712429322544));
+
+    Marker mar13 = Marker(
+      markerId: MarkerId('RIYAZ EXCHANGE TRADING'),
+      draggable: false,
+      infoWindow: InfoWindow(title: 'RIYAZ EXCHANGE TRADING', snippet: 'Boon Lay Shopping Centre, 1 Jelebu Road #02-K4 S(677743)'),
+      position: LatLng(1.38054245051158,103.763993948044));
+    
+    Marker mar14 = Marker(
+      markerId: MarkerId('FAIZURA TRADING'),
+      draggable: false,
+      infoWindow: InfoWindow(title: 'FAIZURA TRADING', snippet: '3155 Commonwealth Avenue West #B1-10A S(129588)'),
+      position: LatLng(1.31485321802259,103.764248205662));
+
+    Marker mar15 = Marker(
+      markerId: MarkerId('JULI PTE LTD'),
+      draggable: false,
+      infoWindow: InfoWindow(title: 'JULI PTE LTD', snippet: '130 Jurong Gateway Road #01-221 S(600130)'),
+      position: LatLng(1.33472490438725,103.73932126848));
+
+    Marker mar16 = Marker(
+      markerId: MarkerId('COMPASS FOREIGN EXCHANGE'),
+      draggable: false,
+      infoWindow: InfoWindow(title: 'COMPASS FOREIGN EXCHANGE', snippet: '1 Sengkang Square #04-12 S(545078)'),
+      position: LatLng(1.39209415018227,103.894980032554));
+
+    
+
+
+
+
+
+
+}
---
+++ lib/screens/map_screen.dart
@@ -0,0 +1,152 @@
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'dart:async';
+import 'package:craftrip_app/models/MapData.dart';
+
+class MapScreen extends StatefulWidget {
+  @override
+  _MapScreenState createState() => _MapScreenState();
+}
+
+class _MapScreenState extends State<MapScreen> {
+  List<Marker> allMarkers = [];
+  Completer<GoogleMapController> _controller = Completer();
+
+  var data = new MapData();
+
+  static final CameraPosition start = CameraPosition(
+    target: LatLng(1.3510, 103.8198),
+    zoom: 10.0,
+  );
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: PreferredSize(
+          preferredSize: Size.fromHeight(65.0),
+          child: AppBar(
+            automaticallyImplyLeading: true,
+            leading: IconButton(
+                icon: Icon(Icons.arrow_back),
+                onPressed: () => Navigator.pop(context, false),
+                iconSize: 20.0,
+                color: Colors.white),
+            backgroundColor: Color(0xff2675eb),
+            title: Center(
+                child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Text('CURRENCY EXCHANGE',
+                    style: TextStyle(
+                        fontSize: 28.0,
+                        letterSpacing: 1.5,
+                        color: Colors.white,
+                        fontWeight: FontWeight.w400)),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    SizedBox(
+                      height: 20.0,
+                      width: 30.0,
+                      child: Image.asset('assets/TravelDiaryIcon.png'),
+                    ),
+                    Text('CrafTrip',
+                        style: TextStyle(
+                            fontWeight: FontWeight.w300,
+                            fontSize: 13.0,
+                            color: Colors.white))
+                  ],
+                ),
+              ],
+            )),
+          )),
+      bottomNavigationBar: new Theme(
+        data: Theme.of(context).copyWith(
+            canvasColor: Colors.black,
+            primaryColor: Color(0xff2675eb),
+            textTheme: Theme.of(context)
+                .textTheme
+                .copyWith(caption: new TextStyle(color: Colors.yellow))),
+        child: BottomNavigationBar(
+          showSelectedLabels: false,
+          showUnselectedLabels: false,
+          items: [
+            new BottomNavigationBarItem(
+                icon: Icon(Icons.home, size: 40.0), title: Text("")),
+            new BottomNavigationBarItem(
+                icon: Icon(Icons.history, size: 40.0), title: Text("")),
+            new BottomNavigationBarItem(
+                icon: Icon(Icons.beenhere, size: 40.0), title: Text("")),
+            new BottomNavigationBarItem(
+                icon: Icon(Icons.favorite, size: 40.0), title: Text(""))
+          ],
+        ),
+      ),
+      body: Center(
+          child: Container(
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height,
+              padding: EdgeInsets.all(10.0),
+              alignment: Alignment(1.0, 3.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Padding(
+                          padding: EdgeInsets.all(10.0),
+                          child: Text('NEAREST MONEY CHANGERS',
+                              style: TextStyle(
+                                  fontSize: 20.0,
+                                  color: Colors.black,
+                                  fontWeight: FontWeight.w400)))
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Container(
+                          width: 360.0,
+                          height: 200.0,
+                          child: GoogleMap(
+                              initialCameraPosition: start,
+                              markers: Set.from([
+                                data.mar1,
+                                data.mar2,
+                                data.mar3,
+                                data.mar4,
+                                data.mar5,
+                                data.mar6,
+                                data.mar7,
+                                data.mar8,
+                                data.mar9,
+                                data.mar10,
+                                data.mar11,
+                                data.mar12,
+                                data.mar13,
+                                data.mar14,
+                                data.mar15,
+                                data.mar16
+                              ]),
+                              onMapCreated: (GoogleMapController controller) {
+                                _controller.complete(controller);
+                              }))
+                    ],
+                  )
+                ],
+              ))),
+    );
+  }
+}
