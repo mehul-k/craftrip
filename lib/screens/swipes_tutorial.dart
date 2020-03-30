4868cf50ce6567f7728dc991a4939a76f134ed7e
Anusha Datta <anushadatta@gmail.com>
2020-03-30 18:41:57 +0800

    Clean up

--- lib/screens/swipes_tutorial.dart
+++ lib/screens/swipes_tutorial.dart
@@ -12,56 +12,7 @@ class _SwipesTutorialState extends State<SwipesTutorial> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // appBar: AppBar(
-      //     title: Column(
-      //       children: <Widget>[
-      //         Padding(
-      //             padding: const EdgeInsets.fromLTRB(0.0, 0.0, 0.0, 0.0),
-      //             child: Text('TRAVEL PICKS TUTORIAL',
-      //               style: TextStyle(
-      //                   fontSize: 26
-      //               ),)),
-      //         Padding(
-      //           padding: const EdgeInsets.fromLTRB(0.0, 5.0, 0.0, 0.0),
-      //           child:
-      //           Row(
-      //             mainAxisAlignment: MainAxisAlignment.center,
-      //             children: <Widget>[
-      //               SizedBox(
-      //                 height: 15.0,
-      //                 width:30.0,
-      //                 child: Image.asset('assets/TravelDiaryIcon.png'),
-      //               ),
-      //               Text('PizzaRush',
-      //                 style: TextStyle(
-      //                     fontSize: 10
-      //                 ),),
-      //             ],
-      //           ),)
-
-      //       ],
-      //     ),
-      //     centerTitle: true,
-      //     backgroundColor: Colors.green[800],
-
-
-      //     actions: <Widget>[
-      //       IconButton(
-      //         icon: Icon(
-      //           Icons.account_circle,
-      //           color: Colors.white,
-      //         ),
-      //         onPressed: () {
-      //           // Should go to User Account page
-      //           // UserAccount()
-      //           Navigator.push(
-      //             context,
-      //             MaterialPageRoute(builder: (context) => UserAccount()),
-      //           );
-      //         },
-      //       )
-      //     ],
-      //   ),
+      
       body: Container(
         decoration: BoxDecoration(
           image: DecorationImage(
